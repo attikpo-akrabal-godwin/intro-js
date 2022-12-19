7d61bac2980dd575549c5faef6dcd899310bf518
akrabal <attikpoakrabal@gmail.com>
2022-12-19 15:36:31 +0000

    add responsive

--- scripts.js
+++ scripts.js
@@ -1,24 +1,42 @@
 let btnA = document.getElementById("btn-a")
 let btnB = document.getElementById("btn-b")
 let btnC = document.getElementById("btn-c")
+let btnE = document.getElementById("btn-e")
+let barSvg = document.getElementById("bar-svg")
+
 
 let modaleContainer = document.getElementById("modale")
 let SpamClose =  document.getElementById("close")
+let SpamCloseSecond =  document.getElementById("close-second-header")
 let modaleForm = document.getElementById("modale-form")
+let secondHeader = document.getElementById("second-header")
+
 
 btnA.addEventListener("click",function(){
     modaleContainer.style.visibility= 'visible'
 });
 
-SpamClose.addEventListener("click",function(){
+btnE.addEventListener("click",function(){
+    modaleContainer.style.visibility= 'visible'
+});
+
+SpamClose.addEventListener("click",()=>{
     modaleContainer.style.visibility= 'hidden'
 })
 
-btnC.addEventListener("click",function(){
+btnC.addEventListener("click",()=>{
     modaleContainer.style.visibility= 'hidden'
 })
 
-btnB.addEventListener("click",function(e){
+btnB.addEventListener("click",(e) =>{
     e.preventDefault()
     modaleForm.style.display= 'none'
 });
+
+barSvg.addEventListener("click",(e)=>{
+    secondHeader.classList.toggle("second-header-collapse")
+})
+
+SpamCloseSecond.addEventListener("click",(e)=>{
+    secondHeader.classList.toggle("second-header-collapse")
+})
